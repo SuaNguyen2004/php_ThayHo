12cf48efdf51efdef398f3b2f6a30df794a9d081
‘SuaNguyen2004’ <abc@gmail.com>
2026-05-18 00:20:25 +0700

    ôn tập lần 1 34. - chưa thuộc và sai, thiếu khá nhiều

---
+++ Ôn tập/34.Lab6_UpdateSP_09/model/sanpham.php
@@ -0,0 +1,44 @@
+<?php
+function updatesp($id, $tensp, $gia, $img, $iddm)
+{
+    $conn = connectdb();
+    if ($img == "") {
+        $sql = "UPDATE tbl_sanpham SET tensp='$tensp', gia='$gia', iddm='$iddm'  WHERE id=" . $id;
+    } else {
+        $sql = "UPDATE tbl_sanpham SET tensp='$tensp', gia='$gia', iddm='$iddm', img='$img'  WHERE id=" . $id;
+    }
+    // execute the query
+    $conn->exec($sql);
+}
+function getone_sp($id)
+{
+    $conn = connectdb();
+    $stmt = $conn->prepare("SELECT * FROM tbl_sanpham WHERE id=" . $id);
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $kq = $stmt->fetchAll();
+    return $kq;
+}
+function delsp($id)
+{
+    $conn = connectdb();
+    $sql = "DELETE FROM tbl_sanpham WHERE id=" . $id;
+    $conn->exec($sql);
+}
+function insert_sanpham($iddm, $tensp, $gia, $img)
+{
+    $conn = connectdb();
+    $sql = "INSERT INTO tbl_sanpham (iddm, tensp, gia, img) VALUES ('$iddm', '$tensp', '$gia', '$img')";
+    $conn->exec($sql);
+}
+function getall_sanpham()
+{
+    $conn = connectdb();
+    $stmt = $conn->prepare("SELECT * FROM tbl_sanpham");
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $kq = $stmt->fetchAll();
+    return $kq;
+}
+
+?>
