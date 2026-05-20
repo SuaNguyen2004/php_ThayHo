0a49387188779e6cd27cfe61109b83226913adb2
‘SuaNguyen2004’ <abc@gmail.com>
2026-05-20 22:10:27 +0700

    ôn 38 lần 2 vẫn ổn nên học luôn 38.2

---
+++ Ôn tập/38.Lab8.1_LoadSP&DM/admin/view/updatedmform.php
@@ -0,0 +1,42 @@
+<?php
+global $kq1;
+?>
+<div class="main">
+    <h1>CẬP NHẬT DANH MỤC</h1>
+    <form action="index.php?act=updatedmform" method="post">
+        <input type="text" name="tendm" id="" value="<?= $kq1[0]['tendm'] ?>">
+        <input type="hidden" name="id" value="<?= $kq1[0]['id'] ?>">
+        <input type="submit" name="capnhat" value="Cập nhật">
+    </form>
+    <br>
+
+    <table>
+        <tr>
+            <th>STT</th>
+            <th>Tên danh mục</th>
+            <th>Ưu tiên</th>
+            <th>Hiển thị</th>
+            <th>Hành động</th>
+        </tr>
+        <?php
+        // var_dump($kq);
+        
+        ?>
+        <?php
+        if (isset($kq) && (count($kq) > 0)) {
+            $i = 1;
+            foreach ($kq as $dm) {
+                echo '<tr>
+                        <td>' . $i . '</td>
+                        <td>' . $dm['tendm'] . '</td>
+                        <td>' . $dm['uutien'] . '</td>
+                        <td>' . $dm['hienthi'] . '</td>
+                        <td><a href="index.php?act=updatedmform&id=' . $dm['id'] . '">Sửa</a> | <a href="index.php?act=deldm&id=' . $dm['id'] . '">Xoá</a></td>
+                    </tr>';
+                $i++;
+            }
+        }
+        ?>
+
+    </table>
+</div>
---
+++ Ôn tập/38.Lab8.1_LoadSP&DM/view/dangky.php
@@ -0,0 +1,24 @@
+<div class="banner1">
+    <div class="menu-draww"></div>
+    <img src="./view/image/coffee.jpg" alt="">
+</div>
+<div class="content1">
+    <div class="boxform">
+        <form action="index.php?act=login" method="post">
+            <h1 style="color:red">ĐĂNG KÝ THÀNH VIÊN</h1><br>
+
+            <div class="container">
+                <label for="email">Email</label><br>
+                <input type="text" placeholder="Enter Email" name="user" value="" required><br>
+
+                <label for="uname">Username</label><br>
+                <input type="text" placeholder="Enter Username" name="user" value="" required><br>
+
+                <label for="psw">Password</label><br>
+                <input type="password" placeholder="Enter Password" name="pass" value="" required>
+                <button type="submit">Login</button>
+            </div>
+
+        </form>
+    </div>
+</div>
