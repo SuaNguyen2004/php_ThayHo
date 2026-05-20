79ef09b895c9b23593da5df3d3b82b964605843c
‘SuaNguyen2004’ <abc@gmail.com>
2026-05-20 16:08:23 +0700

    ôn tập lần 3 37. ngon luôn

---
+++ Ôn tập/37.Lab7.1_Login-RegisterOfUser/view/dangky.php
@@ -0,0 +1,25 @@
+<div class="banner1">
+    <div class="menu-draww"></div>
+    <img src="./view/image/coffee.jpg" alt="">
+</div>
+<div class="content1">
+    <div class="boxform">
+        <form action="index.php?act=login" method="post">
+            <h1 style="color:red">ĐĂNG KÝ THÀNH VIÊN</h1><br>
+
+            <div class="container">
+                <label for="email">Email</label><br>
+                <input type="text" placeholder="Enter Email" name="user" value="" required><br>
+
+                <label for="uname">Username</label><br>
+                <input type="text" placeholder="Enter Username" name="user" value="" required><br>
+
+                <label for="psw">Password</label><br>
+                <input type="password" placeholder="Enter Password" name="pass" value="" required>
+                <br>
+                <input type="submit" name="register" value="Đăng ký">
+            </div>
+
+        </form>
+    </div>
+</div>
